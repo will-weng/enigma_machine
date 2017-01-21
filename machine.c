ff259323003f70d82b4a820e27b3a4eaac249aaf
wilzlerr <william.weng@hotmail.com>
2017-01-21 12:32:30 +1100

    fully working

--- machine.c
+++ machine.c
@@ -6,21 +6,31 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "enigma.h"
 
 int main(int argc, char *argv[]) {
     // copies the message into an array to decrypt or encrypt later;
-    char message[] = "jsdff";
-    int length = 5;
+    if(argc < 2) {
+        printf("Incorrect number of arguments\nUsage: ./enigmaMachine message\n");
+        return EXIT_FAILURE;
+    } else {
+        int length = strlen(argv[1]);
+        char message[length + 1];
+        if(!sscanf(argv[1], "%s", message)) {
+            printf("Invalid input\n");
+            return EXIT_FAILURE;
+        }
+        
+        Setting e = newEnigma();
+        int i;
 
-    Setting e = newEnigma();
-    int i;
-    for(i = 0; i < length; i++) {
-        printf("%c", scramble(message[i], e));
-    }
-    printf("\nis the message");
-
-    deleteEnigma(e);
-    
+        printf("The encrypted/decrypted message is:\n");
+        for(i = 0; i < length; i++) {
+            printf("%c", scramble(message[i], e));
+        }
+        printf("\n");
+        deleteEnigma(e);
+    }    
     return EXIT_SUCCESS;
 }
